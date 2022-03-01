51e0b4415b9904ca1e650262d60b4b4b9c5d39b5
Tomi Mester <mestitomi@gmail.com>
2022-03-01 19:20:44 +0100

    Create log_frontend_pageview.js

---
+++ log_frontend_pageview.js
@@ -0,0 +1,25 @@
+<script>
+if(!document.cookie.includes("d36ct")){
+ var cn36 = "d36ct";
+ var cv36 = Math.floor(Math.random()*1000000000000000000000);
+ var et36 = 2592000 * 12;
+ et36 = et36 * 1000;
+ var date = new Date(); 
+ var dateTimeNow = date.getTime(); 
+ date.setTime(dateTimeNow + et36);
+ var date = date.toUTCString();
+ document.cookie = cn36+"="+cv36+"; expires="+date+"; path=/; domain=." + location.hostname.replace(/^www\./i, "");
+}
+  
+cn36="d36ct"
+var cs36=RegExp(cn36+"=[^;]+").exec(document.cookie);
+cv36=decodeURIComponent(!!cs36 ? cs36.toString().replace(/^[^=]+./,"") : "");
+dts36=[cv36, document.referrer, document.location.href, screen.width, screen.height, screen.availHeight, screen.availWidth].join('nehezazelet');
+var h36 = new XMLHttpRequest();
+var u36 = "https://yourdataservers-domain.eu/d36t/";
+var p36 = "pageview_blogsomesmartfieldseparator" + dts36;
+h36.open("POST", u36, true);
+h36.setRequestHeader('User-Type', 'none');
+h36.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+h36.send(p36);
+</script>
