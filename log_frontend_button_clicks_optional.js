4110d395750e3409a9abe976b3a43337dc78aa24
Tomi Mester <mestitomi@gmail.com>
2022-03-01 19:25:24 +0100

    Create log_frontend_button_clicks_optional.js

---
+++ log_frontend_button_clicks_optional.js
@@ -0,0 +1,13 @@
+<script>
+cn36="d36ct"
+var cs36=RegExp(cn36+"=[^;]+").exec(document.cookie);
+cv36=decodeURIComponent(!!cs36 ? cs36.toString().replace(/^[^=]+./,"") : "");
+dts36=[cv36, {{Click Text}}+"_"+{{Click ID}}+"_"+{{Click URL}}, document.location.href, screen.width, screen.height, screen.availHeight, screen.availWidth].join('nehezazelet');
+var h36 = new XMLHttpRequest();
+var u36 = "https://yourdataservers_domain.eu/d36t/";
+var p36 = "button_clicknsomesmartfieldseparator" + dts36;
+h36.open("POST", u36, true);
+h36.setRequestHeader('User-Type', 'none');
+h36.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+h36.send(p36);
+</script>
